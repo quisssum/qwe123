474316a8f1e957c364d8c53eea05c75e3dae7c16
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-05-31 16:44:24 +0300

    Add function to tokenize text

--- 3b1b1a/SimpleTextGenerationTask.py
+++ 3b1b1a/SimpleTextGenerationTask.py
@@ -4,3 +4,6 @@ def load_book(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
     return text
+
+def tokenize(text):
+    return text.lower().split()
