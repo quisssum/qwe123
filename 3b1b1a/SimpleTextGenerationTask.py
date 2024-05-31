7e6a65455938a6f7e6260341abdabb74bd065ca0
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-05-31 17:10:17 +0300

    Add function to build Markov Chain dictionary

--- 3b1b1a/SimpleTextGenerationTask.py
+++ 3b1b1a/SimpleTextGenerationTask.py
@@ -7,3 +7,13 @@ def load_book(file_path):
 
 def tokenize(text):
     return text.lower().split()
+
+def build_markov_chain(tokens):
+    markov_chain = {}
+    for i in range(len(tokens) - 1):
+        current_token = tokens[i]
+        next_token = tokens[i + 1]
+        if current_token not in markov_chain:
+            markov_chain[current_token] = []
+        markov_chain[current_token].append(next_token)
+    return markov_chain
